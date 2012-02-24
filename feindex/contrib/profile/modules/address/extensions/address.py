22102bff5381bad33d12b31e6528951ec1364e7f
Mark Renton <indexofire@gmail.com>
2012-02-24 02:47:28 -0600

    add address extension

---
+++ feindex/contrib/profile/modules/address/extensions/address.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.contrib import admin
+from contrib.profile.modules.address.models import Address
+
+
+def register(cls, admin_cls):
+    cls.add_to_class(
+        'addresses',
+        models.ManyToManyField(
+            'profiles.Address',
+            related_name='addresses',
+            blank=True,
+        ),
+    )
+
+    if admin_cls:
+        class AddressInline(admin.StackedInline):
+            model = Address
+            extra = 1
+
+        admin_cls.inlines = admin_cls.inlines + [AddressInline]
